42dffa84935bd541c5710265b6230480eae8f0d4
decagon <udemekendrick@gmail.com>
2022-12-23 14:31:39 +0100

    Initial commit

---
+++ src/main/java/com/tequiscode/Tequiscode/TequiscodeApplication.java
@@ -0,0 +1,13 @@
+package com.tequiscode.Tequiscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TequiscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TequiscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tequiscode/Tequiscode/TequiscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tequiscode.Tequiscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TequiscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
